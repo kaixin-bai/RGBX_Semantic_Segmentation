0f09cc26b69591f442ebfe81c36a36a0f92e5984
Kaixin Bai <kb@ar-mps01.ar.int>
2023-10-26 13:08:21 +0200

    add commit

--- train.py
+++ train.py
@@ -26,11 +26,15 @@ from tensorboardX import SummaryWriter
 parser = argparse.ArgumentParser()
 logger = get_logger()
 
+# 设置分布式训练的MASTER_PORT环境变量
 os.environ['MASTER_PORT'] = '169710'
 
+# 启动计算引擎
 with Engine(custom_parser=parser) as engine:
+    # 解析命令行参数
     args = parser.parse_args()
 
+    # 配置CUDA相关设置
     cudnn.benchmark = True
     seed = config.seed
     if engine.distributed:
@@ -39,7 +43,7 @@ with Engine(custom_parser=parser) as engine:
     if torch.cuda.is_available():
         torch.cuda.manual_seed(seed)
 
-    # data loader
+    # 数据加载器初始化，获取训练数据加载器和样本选择器
     train_loader, train_sampler = get_train_loader(engine, RGBXDataset)
 
     if (engine.distributed and (engine.local_rank == 0)) or (not engine.distributed):
@@ -48,24 +52,20 @@ with Engine(custom_parser=parser) as engine:
         tb = SummaryWriter(log_dir=tb_dir)
         engine.link_tb(tb_dir, generate_tb_dir)
 
-    # config network and criterion
+    # 设置模型和损失函数:设置交叉熵损失函数,根据是否进行分布式训练选择适当的BatchNorm层,初始化模型
     criterion = nn.CrossEntropyLoss(reduction='mean', ignore_index=config.background)
-
     if engine.distributed:
         BatchNorm2d = nn.SyncBatchNorm
     else:
         BatchNorm2d = nn.BatchNorm2d
-    
-    model=segmodel(cfg=config, criterion=criterion, norm_layer=BatchNorm2d)
-    
-    # group weight and config optimizer
+    model = segmodel(cfg=config, criterion=criterion, norm_layer=BatchNorm2d)
+
+    # 分组权重和设置优化器:根据BatchNorm层的权重，对模型权重进行分组,根据配置文件中的参数设置优化器
     base_lr = config.lr
     if engine.distributed:
         base_lr = config.lr
-    
     params_list = []
     params_list = group_weight(params_list, model, BatchNorm2d, base_lr)
-    
     if config.optimizer == 'AdamW':
         optimizer = torch.optim.AdamW(params_list, lr=base_lr, betas=(0.9, 0.999), weight_decay=config.weight_decay)
     elif config.optimizer == 'SGDM':
@@ -73,88 +73,97 @@ with Engine(custom_parser=parser) as engine:
     else:
         raise NotImplementedError
 
-    # config lr policy
+    # 配置学习率策略
     total_iteration = config.nepochs * config.niters_per_epoch
     lr_policy = WarmUpPolyLR(base_lr, config.lr_power, total_iteration, config.niters_per_epoch * config.warm_up_epoch)
 
+    # 分布式训练的配置:根据是否进行分布式训练选择相应的设备，并调整模型
     if engine.distributed:
         logger.info('.............distributed training.............')
         if torch.cuda.is_available():
             model.cuda()
-            model = DistributedDataParallel(model, device_ids=[engine.local_rank], 
+            model = DistributedDataParallel(model, device_ids=[engine.local_rank],
                                             output_device=engine.local_rank, find_unused_parameters=False)
     else:
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         model.to(device)
 
+    # 注册模型、优化器和数据加载器到计算引擎：这行代码向计算引擎注册了数据加载器、模型和优化器。这是为了在训练过程中保持这些组件的状态，便于后续的恢复和管理
     engine.register_state(dataloader=train_loader, model=model,
                           optimizer=optimizer)
+
+    # 恢复之前的训练状态：如果有可继续的训练状态（例如中断后的恢复），则从检查点恢复模型、优化器等的状态。
     if engine.continue_state_object:
         engine.restore_checkpoint()
 
+    # 初始化优化器和模型的状态：清空（重置）优化器的梯度，将模型设置为训练模式（启用BatchNorm和Dropout等）
     optimizer.zero_grad()
     model.train()
     logger.info('begin trainning:')
-    
-    for epoch in range(engine.state.epoch, config.nepochs+1):
+
+    # 开始训练的主循环:
+    for epoch in range(engine.state.epoch, config.nepochs + 1):
+        # 分布式训练的采样器设置：如果在进行分布式训练，更新训练采样器的epoch值，确保每个epoch的数据划分一致
         if engine.distributed:
             train_sampler.set_epoch(epoch)
+        # 设置进度条：初始化进度条，并设置格式
         bar_format = '{desc}[{elapsed}<{remaining},{rate_fmt}]'
         pbar = tqdm(range(config.niters_per_epoch), file=sys.stdout,
                     bar_format=bar_format)
+        # 初始化数据加载器迭代器：将数据加载器转换为迭代器，以便在循环中使用
         dataloader = iter(train_loader)
-
+        # 初始化累计损失
         sum_loss = 0
 
         for idx in pbar:
+            # 更新计算引擎的迭代信息：更新计算引擎中当前的epoch和迭代次数信息
             engine.update_iteration(epoch, idx)
-
+            # 数据加载与预处理：从数据加载器获取一个minibatch，提取并处理需要的数据，如图像、标签等，将数据移动到GPU上进行加速计算
             minibatch = dataloader.next()
             imgs = minibatch['data']
             gts = minibatch['label']
             modal_xs = minibatch['modal_x']
-
             imgs = imgs.cuda(non_blocking=True)
             gts = gts.cuda(non_blocking=True)
             modal_xs = modal_xs.cuda(non_blocking=True)
-
+            # 计算损失：设置辅助损失的比例，通过模型计算损失
             aux_rate = 0.2
             loss = model(imgs, modal_xs, gts)
-
-            # reduce the whole loss over multi-gpu
+            # 多GPU上损失的同步：如果是分布式训练环境，同步所有GPU上的损失
             if engine.distributed:
                 reduce_loss = all_reduce_tensor(loss, world_size=engine.world_size)
-            
+            # 优化器梯度清零，反向传播和优化步骤：重置优化器的梯度，执行反向传播来计算梯度，更新模型参数
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-
-            current_idx = (epoch- 1) * config.niters_per_epoch + idx 
+            # 更新学习率：根据当前迭代次数计算学习率，更新优化器中的学习率
+            current_idx = (epoch - 1) * config.niters_per_epoch + idx
             lr = lr_policy.get_lr(current_idx)
-
             for i in range(len(optimizer.param_groups)):
                 optimizer.param_groups[i]['lr'] = lr
-
+            # 打印训练信息：累计损失，根据是否是分布式训练，打印相应的训练信息
             if engine.distributed:
                 sum_loss += reduce_loss.item()
                 print_str = 'Epoch {}/{}'.format(epoch, config.nepochs) \
-                        + ' Iter {}/{}:'.format(idx + 1, config.niters_per_epoch) \
-                        + ' lr=%.4e' % lr \
-                        + ' loss=%.4f total_loss=%.4f' % (reduce_loss.item(), (sum_loss / (idx + 1)))
+                            + ' Iter {}/{}:'.format(idx + 1, config.niters_per_epoch) \
+                            + ' lr=%.4e' % lr \
+                            + ' loss=%.4f total_loss=%.4f' % (reduce_loss.item(), (sum_loss / (idx + 1)))
             else:
                 sum_loss += loss
                 print_str = 'Epoch {}/{}'.format(epoch, config.nepochs) \
-                        + ' Iter {}/{}:'.format(idx + 1, config.niters_per_epoch) \
-                        + ' lr=%.4e' % lr \
-                        + ' loss=%.4f total_loss=%.4f' % (loss, (sum_loss / (idx + 1)))
-
+                            + ' Iter {}/{}:'.format(idx + 1, config.niters_per_epoch) \
+                            + ' lr=%.4e' % lr \
+                            + ' loss=%.4f total_loss=%.4f' % (loss, (sum_loss / (idx + 1)))
+            # 释放loss的内存
             del loss
+            # 更新进度条：设置进度条的描述信息
             pbar.set_description(print_str, refresh=False)
-        
+        # 记录TensorBoard：在分布式训练的主节点或非分布式环境下，将平均损失记录到TensorBoard
         if (engine.distributed and (engine.local_rank == 0)) or (not engine.distributed):
             tb.add_scalar('train_loss', sum_loss / len(pbar), epoch)
-
-        if (epoch >= config.checkpoint_start_epoch) and (epoch % config.checkpoint_step == 0) or (epoch == config.nepochs):
+        # 保存checkpoint：根据配置的条件（比如达到某个epoch或达到保存频率），保存模型的检查点
+        if (epoch >= config.checkpoint_start_epoch) and (epoch % config.checkpoint_step == 0) or (
+                epoch == config.nepochs):
             if engine.distributed and (engine.local_rank == 0):
                 engine.save_and_link_checkpoint(config.checkpoint_dir,
                                                 config.log_dir,
--- utils/init_func.py
+++ utils/init_func.py
@@ -7,6 +7,7 @@
 import torch
 import torch.nn as nn
 
+
 def __init_weight(feature, conv_init, norm_layer, bn_eps, bn_momentum,
                   **kwargs):
     for name, m in feature.named_modules():
@@ -31,28 +32,42 @@ def init_weight(module_list, conv_init, norm_layer, bn_eps, bn_momentum,
 
 
 def group_weight(weight_group, module, norm_layer, lr):
+    """
+    将一个神经网络模型中的参数分组，以便在优化器中对不同的参数组应用不同的学习率和权重衰减（weight decay）。这种方法通常用于实现对某些层（如批归一化层）使用不同的正则化策略
+    这个函数在实践中非常有用，尤其是当使用诸如Adam之类的优化器时，对于不同类型的层使用不同的正则化策略可以提高模型性能
+    """
+    # 定义分组列表：group_decay 用于存储需要应用权重衰减的参数；group_no_decay 用于存储不需要权重衰减的参数
     group_decay = []
     group_no_decay = []
+    # 初始化计数变量：初始化一个计数器，但在此函数中似乎未被使用
     count = 0
+    # 遍历模型中的所有模块：遍历传入模型中的所有子模块
     for m in module.modules():
+        # 处理线性层（全连接层）：如果模块是线性层（全连接层），则将其权重添加到需要衰减的组中；如果这个层有偏置，则将偏置添加到不需要衰减的组中
         if isinstance(m, nn.Linear):
             group_decay.append(m.weight)
             if m.bias is not None:
                 group_no_decay.append(m.bias)
+        # 处理卷积层：如果模块是卷积层（包括传统卷积层和转置卷积层），类似地处理其权重和偏置
         elif isinstance(m, (nn.Conv1d, nn.Conv2d, nn.Conv3d, nn.ConvTranspose2d, nn.ConvTranspose3d)):
             group_decay.append(m.weight)
             if m.bias is not None:
                 group_no_decay.append(m.bias)
+        # 处理归一化层：如果模块是归一化层（包括自定义的归一化层和常见的归一化类型），将其参数放入不需要衰减的组
         elif isinstance(m, norm_layer) or isinstance(m, nn.BatchNorm1d) or isinstance(m, nn.BatchNorm2d) \
                 or isinstance(m, nn.BatchNorm3d) or isinstance(m, nn.GroupNorm) or isinstance(m, nn.LayerNorm):
             if m.weight is not None:
                 group_no_decay.append(m.weight)
             if m.bias is not None:
                 group_no_decay.append(m.bias)
+        # 处理其他 nn.Parameter 类型参数：如果模块是 nn.Parameter 类型，将其添加到需要衰减的组中
         elif isinstance(m, nn.Parameter):
             group_decay.append(m)
-   
+    # 断言：确保所有参数都被考虑
     assert len(list(module.parameters())) >= len(group_decay) + len(group_no_decay)
+    # 添加需要衰减的参数组：将需要衰减的参数组添加到 weight_group 中，并指定学习率
     weight_group.append(dict(params=group_decay, lr=lr))
+    # 添加不需要衰减的参数组：将不需要衰减的参数组添加到 weight_group 中，设置 weight_decay 为 0，指定学习率
     weight_group.append(dict(params=group_no_decay, weight_decay=.0, lr=lr))
+    # 返回更新后的参数组：返回更新后的 weight_group，包含两种类型的参数组
     return weight_group
